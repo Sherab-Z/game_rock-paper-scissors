5081e207fd7434e00dba22a08c2d7fe894a3df26
Sherab-Z <codebodhi34@gmail.com>
2023-01-10 20:02:39 +0100

    Working game, but bug remains in handling of invalid user input

--- pseudo.md
+++ pseudo.md
@@ -1,7 +1,22 @@
 # Rock Paper Scissors Game
 
 ## Overview
-- When the user opens a new game (by opening or refreshing the page), the computer makes a random choice of rock, paper or scissors; 
-- then prompt the user to make their choice; 
-- then compare the user's choice with its own, and based on conditional logic, determine the winner; 
-- then alert this result to the user.
+
+- When the user opens a new game (by opening or refreshing the page), the computer makes a random choice of rock, paper or scissors;
+- Play a round:
+  - prompt the user to make their choice;
+  - convert this to lowercase and store in the variable userChoice;
+  - compare the user's choice with computer's based on conditional logic and alert user about who is the winner:
+    - for each allowed user choice:
+      - compare to computerChoice;
+      - store the result in the variable roundResult;
+      - alert the result of the round to the user;
+      - if user's choice is invalid, alert error message to the user;
+- Repeat until 5 rounds are completed:
+  - store the scores along the way in 2 variables;
+  - after the 5th round, compare scores and alert the overall winner to the user;
+
+NOTES:
+currently debugging playSingleRound(). It gives a winner, but doesn't return a number (-1, 0, or 1) as expected...?
+
+- use dev tools breakpoints to debug
--- script.js
+++ script.js
@@ -1,18 +1,115 @@
-// When the user opens a new game (by opening or refreshing the page), the computer makes a random choice of rock, paper or scissors; 
+// When the user opens a new game (by opening or refreshing the page), the computer makes a random choice of rock, paper or scissors;
 function getRandomChoice() {
   let i = Math.floor(Math.random() * 3);
-  let computerChoice;
   switch (i) {
     case 0:
-      computerChoice = "rock";
+      computerSelection = "rock";
       break;
     case 1:
-      computerChoice = "paper";
+      computerSelection = "paper";
       break;
     case 2:
-      computerChoice = "scissors";
+      computerSelection = "scissors";
       break;
     default:
-      computerChoice = "The Computer has'nt made a choice";
+      computerSelection = "The Computer has'nt made a choice";
+  }
+  return computerSelection;
+}
+
+function playSingleRound() {
+  // get computer's choice
+  getRandomChoice();
+  // prompt the user to make their choice
+  let playerSelection = prompt(
+    "Choose rock, paper, or scissors!"
+  ).toLowerCase();
+  // compare the user's choice with its own, and based on conditional logic, determine the winner;
+  let roundResult;
+
+  switch (playerSelection) {
+    case "rock":
+      if (computerSelection === "rock") {
+        console.log("szcl: round complete!");
+        alert("It's a tie.");
+        return (roundResult = 0);
+      } else if (computerSelection === "paper") {
+        console.log("szcl: round complete!");
+        alert(`You lose - ${computerSelection} beats ${playerSelection}.`);
+        return (roundResult = -1);
+      } else if (computerSelection === "scissors") {
+        console.log("szcl: round complete!");
+        alert(`You win! ${playerSelection} beats ${computerSelection}`);
+        return (roundResult = 1);
+      } else {
+        alert("Please choose either 'rock', 'paper' or 'scissors'.");
+        return (roundResult = null);
+      }
+      break;
+
+    case "paper":
+      if (computerSelection === "rock") {
+        console.log("szcl: round complete!");
+        alert(`You win! ${playerSelection} beats ${computerSelection}`);
+        return (roundResult = 1);
+      } else if (computerSelection === "paper") {
+        console.log("szcl: round complete!");
+        alert("It's a tie.");
+        return (roundResult = 0);
+      } else if (computerSelection === "scissors") {
+        console.log("szcl: round complete!");
+        alert(`You lose - ${computerSelection} beats ${playerSelection}.`);
+        return (roundResult = -1);
+      } else {
+        alert("Please choose either 'rock', 'paper' or 'scissors'.");
+        return (roundResult = null);
+      }
+      break;
+
+    case "scissors":
+      if (computerSelection === "rock") {
+        console.log("szcl: round complete!");
+        alert(`You lose - ${computerSelection} beats ${playerSelection}.`);
+        return (roundResult = -1);
+      } else if (computerSelection === "paper") {
+        console.log("szcl: round complete!");
+        alert(`You win - ${playerSelection} beats ${computerSelection}!`);
+        return (roundResult = 1);
+      } else if (computerSelection === "scissors") {
+        console.log("szcl: round complete!");
+        alert("It's a tie.");
+        return (roundResult = 0);
+      } else {
+        alert("Please choose either 'rock', 'paper' or 'scissors'.");
+        return (roundResult = null);
+      }
+      break;
+  }
+}
+
+function game() {
+  let computerSelection = null;
+
+  let playerOverallScore = 0;
+
+  let i = 0;
+  while (i < 5) {   // play game 5 times, keeping track of player's overall score
+    let roundScore = playSingleRound();
+    if (roundScore === null) {
+      alert("Please replay the round...");
+    } else {
+      playerOverallScore += roundScore;
+      i--;
+    }
+  }
+
+  if (playerOverallScore > 0) {
+    alert("Congratulations! You won the game!!!");
+  } else if (playerOverallScore < 0) {
+    alert("Sorry, you lost the game. Better luck next time!"); //
+  } else if (playerOverallScore === 0) {
+    alert("It's a tie. Want to try again?");
+  } else {
+    alert("Something went wrong...no result from the game.");
   }
 }
